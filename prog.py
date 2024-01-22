b2c09d49ecbab6b88ad1fd3493f09f1bf935a25e
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 14:23:35 +0800

    trying the recommended solution, the recommended solution was better and more advance

--- prog.py
+++ prog.py
@@ -2,13 +2,15 @@ import tkinter as tk
 from tkinter import simpledialog
 
 
-word = str(input("What is your word: "))
+#FOLLOWING THE RECOMMENDED SOLUTION ##############################################################################################################################################################################################################################
+recomm_word = str(input("What is your word: "))
+recomm_w_length = len(recomm_word)
+recomm_num_of_r_letters = tk.simpledialog.askinteger("Please answer","How many letters do you want to remove", maxvalue = (recomm_w_length - 1))
 
-word_length = len(word)
+def remove_chars(word_2, number):
+    print('Original string:', word_2)
+    value = word_2[number:]
+    return value
 
-number_of_r_letter = tk.simpledialog.askinteger("Please answer","How many letters do you want to delete", maxvalue = (word_length - 1))
-
-for letters in range(word_length):
-    letters = number_of_r_letter
-    print(word[letters])
-    number_of_r_letter = number_of_r_letter + 1 
+print("Removing characters from a string")
+print(remove_chars(recomm_word, recomm_num_of_r_letters))
