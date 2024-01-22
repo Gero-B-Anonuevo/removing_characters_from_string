033dc89536cae46a3198e4e07069045a9b3b51b0
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 14:32:56 +0800

    git bash updating the message of commit but not the code again

--- prog.py
+++ prog.py
@@ -1,7 +1,16 @@
 import tkinter as tk 
 from tkinter import simpledialog
 
+word = str(input("What is your word: "))
 
+word_length = len(word)
+
+number_of_r_letter = tk.simpledialog.askinteger("Please answer","How many letters do you want to delete", maxvalue = (word_length - 1))
+
+for letters in range(word_length):
+    letters = number_of_r_letter
+    print(word[int(letters)])
+    number_of_r_letter = number_of_r_letter + 1 
 #FOLLOWING THE RECOMMENDED SOLUTION ##############################################################################################################################################################################################################################
 recomm_word = str(input("What is your word: "))
 recomm_w_length = len(recomm_word)
@@ -14,3 +23,7 @@ def remove_chars(word_2, number):
 
 print("Removing characters from a string")
 print(remove_chars(recomm_word, recomm_num_of_r_letters))
+
+#again the gitbash is not working
+
+print("try")
